f4a366654446f00ff56778bbbc4ca7f4c3b174c0
toidiu <apoorv@toidiu.com>
2020-01-05 14:30:20 -0800

    proj 05

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,41 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, prelude::*, Delay, Leds};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, Leds) = aux5::init();
 
     // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let half_period = 100_u16;
+
+    let mut idx: usize = 0;
+    clear_leds(&mut leds);
+    loop {
+        // clear_leds(&mut leds);
+        turn_off_led(&mut leds, idx);
+        increment_idx(&mut idx);
+        turn_on_led(&mut leds, idx);
+
+        delay.delay_ms(half_period);
+    }
+}
+
+fn increment_idx(idx: &mut usize) {
+    *idx = (*idx + 1) % 8;
+}
+
+fn clear_leds(leds: &mut Leds) {
+    for idx in 0..8 {
+        leds[idx].off();
+    }
+}
+
+fn turn_off_led(leds: &mut Leds, idx: usize) {
+    leds[idx].off();
+}
+
+fn turn_on_led(leds: &mut Leds, idx: usize) {
+    leds[idx].on();
 }
